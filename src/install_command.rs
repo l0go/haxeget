89ef08981f7115ad200f5831149ebb3398c5f320
AltronMaxX <max06112004@gmail.com>
2023-12-17 01:30:06 +0400

    download nightly

--- src/install_command.rs
+++ src/install_command.rs
@@ -17,8 +17,12 @@ pub async fn run_install(version: String) -> Result<()> {
     }
 
     // Downloads the haxe .tar.gz file
-    let download = executor::block_on(download::from_github(&cache, &version));
-
+    let download: std::prelude::v1::Result<String, color_eyre::eyre::Error> = if version.eq("nightly") {
+        executor::block_on(download::downloadnightly(&cache))
+    }  else {
+        executor::block_on(download::from_github(&cache, &version))
+    };
+    
     // If download was successful, we will extract the tarball and store the version
     if let Ok(file_name) = download {
         let location = cache.get_haxe_directory_from_tar(&file_name).unwrap();
